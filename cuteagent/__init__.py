23ad7bff5bcb2cb86fa7758b929e04df30fbad66
MasoudJB <masoud@fintor.co>
2025-06-17 18:08:17 -0700

    Bump version: 0.0.16 → 0.0.17

--- cuteagent/__init__.py
+++ cuteagent/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Masoud Jb"""
 __email__ = "eng@fintor.com"
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 
 from .cuteagent import WindowsAgent, VisionAgent, HumanAgent, StationAgent
 
