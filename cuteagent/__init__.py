bd8deb9709bfe837fb286dd61038782a11b86387
MasoudJB <masoud@fintor.co>
2025-06-18 10:07:34 -0700

    Release v0.2.0

--- cuteagent/__init__.py
+++ cuteagent/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Masoud Jb"""
 __email__ = "eng@fintor.com"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from .cuteagent import WindowsAgent, VisionAgent, HumanAgent, StationAgent
 
