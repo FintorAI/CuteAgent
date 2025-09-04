e85bc9906e7dd1c43fcbb637b6c843330b75c5a8
antonluisboquer <anton.boquer@cybersoftbpo.com>
2025-09-04 16:51:28 +0800

    Release v0.2.21

--- cuteagent/__init__.py
+++ cuteagent/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Masoud Jb"""
 __email__ = "eng@fintor.com"
-__version__ = "0.2.20"
+__version__ = "0.2.21"
 
 from .cuteagent import WindowsAgent, VisionAgent, HumanAgent, StationAgent, DocumentAgent
 
